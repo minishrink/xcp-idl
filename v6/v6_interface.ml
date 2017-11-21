36c113d50a5afdbe04dc32f428a550c99113206b
Akanksha Mathur <akanksha.mathur@citrix.com>
2017-11-21 16:35:01 +0000

    CP:26098: Wrap errors and types, define ppx-based RPC functor
    [v6d] Define functor to generate RPC code
    
    Note: Functor was originally just called API, changed this to prevent
    namespace clashes with an autogenerated module titled API
    
    [v6d] Type functions and parameters for PPX code generation

--- v6/v6_interface.ml
+++ v6/v6_interface.ml
@@ -12,6 +12,9 @@
  * GNU Lesser General Public License for more details.
  *)
 
+open Rpc
+open Idl
+
 let service_name = "v6d"
 let queue_name = ref (Xcp_service.common_prefix ^ service_name)
 let default_sockets_dir = "/var/lib/xcp"
@@ -19,26 +22,70 @@ let default_path = ref (Filename.concat default_sockets_dir service_name)
 let uri () = "file:" ^ !default_path
 
 type debug_info = string
+(** Uninterpreted/sanitised string associated with operation *)
+[@@deriving rpcty]
 
 type edition_info = {
-    edition: string;
-    xapi_params: (string * string) list;
-    additional_params: (string * string) list;
-    experimental_features: (string * bool) list;
+  edition: string;
+  xapi_params: (string * string) list;
+  additional_params: (string * string) list;
+  experimental_features: (string * bool) list;
 }
+[@@deriving rpcty]
 
-exception Invalid_edition of string
-exception V6d_failure
-exception License_expired
-exception License_processing_error
-exception Missing_connection_details
-exception License_checkout_error of string
+type errors =
+  | Invalid_edition of string
+  (** Thrown by apply_edition on receiving unfamiliar edition. Note: get_editions returns list of all valid editions *)
+  | V6d_failure
+  (** Licensing daemon failed *)
+  | License_expired
+  (** Thrown by license_check when expiry date matches or precedes current date *)
+  | License_processing_error
+  (** License could not be processed *)
+  | Missing_connection_details
+  (** Thrown if connection port or address parameter not supplied to check_license *)
+[@@deriving rpcty]
 
-(* dbg_str -> requested edition -> current params -> edition_info *)
-external apply_edition : debug_info -> string -> (string * string) list -> edition_info = ""
+module API(R : RPC) = struct
+  open R
 
-(* dbg_str -> list of editions *)
-external get_editions : debug_info -> (string * (string * string * int)) list = ""
+  (* description of functions *)
+  let description = Interface.{
+      name = "Licensing";
+      namespace = None;
+      description = [
+        "This interface is used by Xapi and V6d to manage ";
+        "XenServer edition licensing of hosts.";
+        ];
+      version=(1,0,0);        }
 
-(* dbg_str -> result *)
-external get_version : debug_info -> string = ""
+  (* define implementation *)
+  let implementation = implement description
+
+  (* define global parameters for API calls *)
+  let debug_info_p = Param.mk ~description:[
+    "An uninterpreted string to associate with the operation."
+    ] Types.string 
+
+  (* dbg_str -> requested edition -> current params -> edition_info *)
+  let apply_edition =
+    declare
+    "apply_edition"
+    ["Checks license info and ensures provided features are compatible."]
+    ( debug_info_p @-> edition_p : string @-> current_params : (string * string) list @-> edition_info )
+
+  (* dbg_str @-> list of editions *)
+  let get_editions =
+    declare
+    "get_editions"
+    ["Gets list of accepted editions."]
+    ( debug_info @-> edition list : (string * (string * string * int)) list )
+
+  (* dbg_str @-> result *)
+  let get_version =
+    declare
+    "get_version"
+    ["Returns version"]
+    ( debug_info @-> result : string )
+
+end
